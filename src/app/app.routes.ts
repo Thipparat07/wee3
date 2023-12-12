4949e8973c2d3c3ebe97e2ff3711815b22f42f1c
Thipparat07 <thipparat4606@gmail.com>
2023-12-12 17:43:15 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,25 @@ import { MainComponent } from './pages/main/main.component';
 import { MemberComponent } from './pages/member/member.component';
 import { ProfileComponent } from './pages/member/profile/profile.component';
 import { ListComponent } from './pages/member/list/list.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
+    
     {
       path: 'member',
       component: MemberComponent,
       children: [
         { path: 'list', component: ListComponent },
         { path: 'profile', component: ProfileComponent },
+       
       ],
+      
     },
+    { path: '**', component: PagenotfoundComponent,
+    children: [
+        { path: 'list', component: ListComponent },
+        { path: 'profile', component: ProfileComponent },
+       
+      ], },
   ];
